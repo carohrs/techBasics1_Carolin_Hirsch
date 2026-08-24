71ddddc2e022dd9506efdf42e299748ae429c360
carohrs <carolin.hirsch@stud.leuphana.de>
2026-08-24 16:27:20 +0200

    - changed the size of the animals - pause button - quit global - constants in capital letters

--- final_assignment/Hungry_Animals.py
+++ final_assignment/Hungry_Animals.py
@@ -1,8 +1,7 @@
 #libraries
 import pygame
 import random
-
-from pygame import color
+import os
 
 #start pygame
 pygame.init()
@@ -11,7 +10,6 @@ pygame.init()
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 FPS = 60
-BGCOLOR = (255,255,255)
 
 #Window
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -25,16 +23,10 @@ font_title = pygame.font.SysFont(None, 70)
 font_small = pygame.font.SysFont(None, 35)
 
 #colors
-WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (128,0,32)
 ORANGE = (255,165,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
 ROSA = (231, 84, 128)
-
-import os
 
 IMAGE_FOLDER ="images"
 
@@ -50,11 +42,11 @@ background_image = load_image("Background.png", SCREEN_WIDTH, SCREEN_HEIGHT)
 
 #animal images
 ANIMAL_IMAGES = {
-    "Monkey": load_image("Monkey.png", 80, 80),
-    "Dog": load_image("Dog.png", 80, 80),
-    "Frog": load_image("Frog.png", 80, 80),
-    "Cat": load_image("Cat.png", 80, 80),
-    "Bunny": load_image("Bunny.png", 80, 80)
+    "Monkey": load_image("Monkey.png", 120, 120),
+    "Dog": load_image("Dog.png", 120, 120),
+    "Frog": load_image("Frog.png", 120, 120),
+    "Cat": load_image("Cat.png", 120, 120),
+    "Bunny": load_image("Bunny.png", 120, 120)
 }
 
 #food images
@@ -77,7 +69,7 @@ class Animal:
         self.speed = 8
         self.color = animal_color
         self.image = ANIMAL_IMAGES[animal_name]
-        self.rect = pygame.Rect(self.x, self.y, 80, 80)  # Pygame Rect as hitbox
+        self.rect = pygame.Rect(self.x, self.y, 120, 120)  # Pygame Rect as hitbox
 
     def move_left(self):
         self.x -= self.speed
@@ -102,7 +94,7 @@ class Food:
         self.y = 0  #start at the top of the screen
         self.speed = fall_speed
         self.image = FOOD_IMAGES[food_name]
-        self.rect = pygame.Rect(self.x, self.y, 40, 40)
+        self.rect = pygame.Rect(self.x, self.y, 50, 50)
 
     def fall(self):
         self.y += self.speed
@@ -112,7 +104,7 @@ class Food:
         surface.blit(self.image, (self.x, self.y))
 
 #ANIMAL OPTIONS for the start screen (name, favorite food, color)
-animal_options = [
+ANIMAL_OPTIONS = [
     {"name": "Monkey", "food": "Banana", "color": (210, 140, 50)},
     {"name": "Dog", "food": "Bone", "color": (160, 100, 50)},
     {"name": "Frog", "food": "Fly", "color": (50, 180, 50)},
@@ -130,7 +122,7 @@ food_list = []
 
 #timer for spawning the food
 spawn_timer = 0
-spawn_delay = 60 #maybe later random
+SPAWN_DELAY = 60 #maybe later random
 
 #score and lives
 score = 0
@@ -138,12 +130,13 @@ lives = 3
 highscore = 0
 
 #level (difficulty)
-base_fall_speed = 4
+BASE_FALL_SPEED = 4
 current_fall_speed = 4 #--> will increase over time
 
 #game states
 START_SCREEN = "start_screen"
 PLAYING = "playing"
+PAUSED = "paused"
 GAME_OVER = "game_over"
 game_state = START_SCREEN #so the game starts in the start screen
 
@@ -155,14 +148,14 @@ def reset_game(chosen_animal):
     lives = 3
     food_list = []
     spawn_timer = 0
-    current_fall_speed = base_fall_speed
+    current_fall_speed = BASE_FALL_SPEED
 
     #create the player with the chosen animal
     player = Animal(
         chosen_animal["name"],
         chosen_animal["food"],
         SCREEN_WIDTH / 2,
-        SCREEN_HEIGHT - 100,
+        SCREEN_HEIGHT - 140,
         chosen_animal["color"]
     )
     game_state = PLAYING
@@ -189,30 +182,39 @@ def handle_events():
      # keyboard action
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            return False
 
         if event.type == pygame.KEYDOWN:
+
+            #Q always quits the game
+            if event.key == pygame.K_q:
+                return False
 
             #start screen choose animal
             if game_state == START_SCREEN:
                 if event.key == pygame.K_1:
-                    reset_game(animal_options[0])  # Monkey
+                    reset_game(ANIMAL_OPTIONS[0])  # Monkey
                 if event.key == pygame.K_2:
-                    reset_game(animal_options[1])  # Dog
+                    reset_game(ANIMAL_OPTIONS[1])  # Dog
                 if event.key == pygame.K_3:
-                    reset_game(animal_options[2])  # Frog
+                    reset_game(ANIMAL_OPTIONS[2])  # Frog
                 if event.key == pygame.K_4:
-                    reset_game(animal_options[3])  # Cat
+                    reset_game(ANIMAL_OPTIONS[3])  # Cat
                 if event.key == pygame.K_5:
-                    reset_game(animal_options[4])  # Bunny
+                    reset_game(ANIMAL_OPTIONS[4])  # Bunny
 
             # if game is over, press R to restart or Q to quit
             if game_state == GAME_OVER:
                 if event.key == pygame.K_r:
                     game_state = START_SCREEN
                     food_list = []
-                if event.key == pygame.K_q:
-                    return  False
+
+            if game_state == PLAYING:
+                if event.key == pygame.K_p:
+                    game_state = PAUSED
+            elif game_state == PAUSED:
+                if event.key == pygame.K_p:
+                    game_state = PLAYING
 
     return True
 
@@ -229,7 +231,7 @@ def move_player():
 def spawn_food():
     global spawn_timer
     spawn_timer += 1
-    if spawn_timer >= spawn_delay:
+    if spawn_timer >= SPAWN_DELAY:
         spawn_timer = 0
 
         #40% chance for the favorite food, otherwise random
@@ -254,7 +256,7 @@ def move_food():
         if food.rect.y < SCREEN_HEIGHT:
             food_still_falling.append(food)
         else:
-            #missing favorite food = - lives
+            #missing favorite food = - score
             if food.name == player.favorite_food:
                 score -= 1
 
@@ -286,7 +288,7 @@ def update_game():
     move_player()
 
     #increase fall speed (difficulty) based on score
-    current_fall_speed = base_fall_speed + (score // 5) #every 5 points, increase fall speed
+    current_fall_speed = BASE_FALL_SPEED + (score // 5) #every 5 points, increase fall speed
 
     spawn_food()
     move_food()
@@ -305,28 +307,30 @@ def update_game():
 #START SCREEN
 def draw_start_screen():
     #title
-    title_text = font_big.render("Hungry Animals: Snack Drop", True, ROSA)
-    subtitle_text = font_small.render("Press 1-4 to choose your animal", True, BLACK)
+    title_text = font_title.render("Hungry Animals: Snack Drop", True, ROSA)
+    subtitle_text = font_small.render("Press 1-5 to choose your animal", True, BLACK)
+    quit_hint = font_small.render("Press Q to quit", True, BLACK)
     screen.blit(title_text, (SCREEN_WIDTH // 2 - title_text.get_width() // 2, 100))
     screen.blit(subtitle_text, (SCREEN_WIDTH // 2 - subtitle_text.get_width() // 2, 200))
+    screen.blit(quit_hint, (SCREEN_WIDTH // 2 - quit_hint.get_width() // 2, 240))
 
     #show all 4 animals
-    for i, animal in enumerate(animal_options):
-        x_pos = 60 + i * 230
-        y_pos= 260
+    for i, animal in enumerate(ANIMAL_OPTIONS):
+        x_pos = 40 + i * 235
+        y_pos= 240
 
         #draw animal
         screen.blit(ANIMAL_IMAGES[animal["name"]], (x_pos, y_pos))
         #draw animal name below
         name_text = font.render(animal["name"], True, BLACK)
-        screen.blit(name_text, (x_pos, y_pos + 90))
+        screen.blit(name_text, (x_pos, y_pos + 130))
         #draw favorite food
         food_text = font_small.render("Loves : " + animal["food"], True, BLACK)
-        screen.blit(food_text, (x_pos, y_pos + 140))
+        screen.blit(food_text, (x_pos, y_pos + 180))
 
         #draw key to press
         key_text = font.render("Press " + str(i + 1), True, RED)
-        screen.blit(key_text, (x_pos, y_pos + 190))
+        screen.blit(key_text, (x_pos, y_pos + 230))
 
 #game state PLAYING
 def draw_playing():
@@ -340,10 +344,24 @@ def draw_playing():
     lives_text = font.render("Lives: " + str(lives), True, BLACK)
     animal_text = font.render("Animal: " + player.name, True, BLACK)
     food_hint = font_small.render("Catch: " + player.favorite_food, True, BLACK)
+    pause_hint = font_small.render("Press P to pause", True, BLACK)
+    quit_hint= font_small.render("Press Q to quit", True, BLACK)
     screen.blit(score_text, (20, 20))
     screen.blit(lives_text, (20, 70))
     screen.blit(animal_text, (20, 120))
     screen.blit (food_hint, (20, 170))
+    screen.blit (pause_hint, (20, 210))
+    screen.blit (quit_hint, (20, 240))
+
+def draw_paused():
+    #game in the background
+    draw_playing()
+
+    paused_text = font_big.render("PAUSED", True, BLACK)
+    continue_text = font.render("Press P to continue", True, BLACK)
+    screen.blit(paused_text, (SCREEN_WIDTH // 2 - paused_text.get_width() // 2, 300))
+    screen.blit(continue_text, (SCREEN_WIDTH // 2 - continue_text.get_width() // 2, 400))
+
 
 def draw_game_over():
     gameover_text = font_big.render("GAME OVER", True, (200, 0, 0))
@@ -367,6 +385,9 @@ def draw_everything():
 
     if game_state == PLAYING:
         draw_playing()
+
+    if game_state == PAUSED:
+        draw_paused()
 
     if game_state == GAME_OVER:
         draw_game_over()
@@ -397,4 +418,5 @@ def main():
 
     pygame.quit()
 
-main()
+if __name__ == "__main__":
+    main()
